c137d1f2d0c0ab74d0c986beaebd4a387cd33cc8
lookzhang <89255371@qq.com>
2018-09-30 01:34:51 +0800

    小程序V4

--- pages/index/index.js
+++ pages/index/index.js
@@ -357,25 +357,25 @@ Page({
   checkMin: function(e) {
 
 
-    // if (this.data.birthday != '910418') {
-    //   var random = Math.floor(Math.random() * 10);
-    //   var info = '自作多情呢';
+    if (this.data.birthday != '910418') {
+      var random = Math.floor(Math.random() * 10);
+      var info = '自作多情呢';
 
-    //   if(random > 7){
-    //     info = '白高兴一场'
-    //   }else if(random >5){
-    //     info = '暗恋我？'
-    //   }else if(random >= 2){
-    //     info = '我只喜欢min'
-    //   }
+      if(random > 7){
+        info = '白高兴一场'
+      }else if(random >5){
+        info = '暗恋我？'
+      }else if(random >= 2){
+        info = '我只喜欢min'
+      }
 
-    //   wx.showToast({
-    //     title: info,
-    //     icon: 'loading',
-    //     duration: 1500
-    //   })
-    //   return;
-    // }
+      wx.showToast({
+        title: info,
+        icon: 'loading',
+        duration: 1500
+      })
+      return;
+    }
 
     wx.showToast({
       title: '终于等到你',

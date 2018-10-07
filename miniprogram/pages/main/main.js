389952985b96bd92a97e134a4d01dda933a19fa1
lookzhang <89255371@qq.com>
2018-10-07 22:15:50 +0800

    fix bug

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -47,7 +47,7 @@ Page({
         })
 
         const back = wx.getBackgroundAudioManager();
-        back.title = src.data.title;
+        back.title = res.data.title;
         back.src = res.data.src;
         console.log('[数据库] [查询记录] 成功: ', res)
       },
